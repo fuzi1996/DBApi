48fcb780fbcc13a8299851b47d387e28194d4f27
fuzi1996 <imfuzi@163.com>
2022-08-30 22:52:38 +0800

    fix(dbapi_ui):修复dbIcon文件名及所在目录问题

---
+++ dbapi-ui/src/components/Common/dbIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style='display: inline-block'>
+    <i v-if='type =="mysql"' class='iconfont icon-my-SQL db' />
+    <i v-if='type =="postgresql"' class='iconfont icon-postgresql db' />
+    <i v-if='type =="hive"' class='iconfont icon-hive db' />
+    <i v-if='type =="sqlserver"' class='iconfont icon-SQLServer db' />
+    <i v-if='type =="clickhouse"' class='iconfont icon-clickhouse2 db' />
+    <i v-if='type =="kylin"' class='iconfont icon-Kylin db' />
+    <i v-if='type =="oracle"' class='iconfont icon-oracle db' />
+    <i v-if='type =="TDengine"' class='iconfont icon-TDengine db' />
+    <i v-if='type =="doris"' class='iconfont icon-doris db' />
+    <i v-if='type =="oceanbase"' class='iconfont icon-oceanbase db' />
+    <i v-if='type =="greenplum"' class='iconfont icon-GreenPlum db' />
+    <i v-if='type =="others"' class='iconfont icon-OTHER db' />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DbIcon',
+  props:{
+    type:{
+      type:String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.db {
+  color: #0698a5;
+  font-size: 18px;
+  font-weight: 500;
+  margin-right: 5px;
+}
+</style>
---
+++ dbapi-ui/src/components/Common/mytabPane.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-show='active'>
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'MytabPane',
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    value: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      flag: false
+    }
+  },
+  computed: {
+    active() {
+      const active = this.$parent.currentName === this.label
+      return active
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
+
