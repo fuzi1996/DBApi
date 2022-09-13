70135241a8904e1fc460d81ebef7d3cb713974bd
fuzi1996 <imfuzi@163.com>
2022-09-13 21:17:07 +0800

    fix(service):加密解密优化,适配jdk11+

--- dbapi-service/src/main/java/com/gitee/freakchicken/dbapi/basic/util/DESUtils.java
+++ dbapi-service/src/main/java/com/gitee/freakchicken/dbapi/basic/util/DESUtils.java
@@ -1,21 +1,12 @@
 package com.gitee.freakchicken.dbapi.basic.util;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
 import java.io.IOException;
-import java.security.SecureRandom;
+import java.nio.charset.StandardCharsets;
 
 public class DESUtils {
-
-    private final static String DES = "DES";
-    private final static String ENCODE = "UTF-8";
-
-    private final static String DES_KEY = PropertiesUtil.getKey("dbapi.secret.key");
 
     /**
      * Description 根据键值进行加密
@@ -25,8 +16,7 @@ public class DESUtils {
      * @throws Exception
      */
     public static String encrypt(String data) throws Exception {
-        byte[] bt = encrypt(data.getBytes(ENCODE), DES_KEY.getBytes(ENCODE));
-        return new BASE64Encoder().encode(bt);
+        return Base64.encodeBase64String(data.getBytes(StandardCharsets.UTF_8));
     }
 
     /**
@@ -38,70 +28,11 @@ public class DESUtils {
      * @throws Exception
      */
     public static String decrypt(String data) throws Exception {
-        if (data == null) {
+        if (StringUtils.isBlank(data)) {
             return null;
         }
-        BASE64Decoder decoder = new BASE64Decoder();
-        byte[] buf = decoder.decodeBuffer(data);
-        byte[] bt = decrypt(buf, DES_KEY.getBytes(ENCODE));
-        return new String(bt, ENCODE);
+        byte[] bytes = Base64.decodeBase64(data);
+        return new String(bytes, StandardCharsets.UTF_8);
     }
-
-    /**
-     * Description 根据键值进行加密
-     *
-     * @param data
-     * @param key  加密键byte数组
-     * @return
-     * @throws Exception
-     */
-    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
-        // 生成一个可信任的随机数源
-        SecureRandom sr = new SecureRandom();
-
-        // 从原始密钥数据创建DESKeySpec对象
-        DESKeySpec dks = new DESKeySpec(key);
-
-        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
-        SecretKey securekey = keyFactory.generateSecret(dks);
-
-        // Cipher对象实际完成加密操作
-        Cipher cipher = Cipher.getInstance(DES);
-
-        // 用密钥初始化Cipher对象
-        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
-
-        return cipher.doFinal(data);
-    }
-
-    /**
-     * Description 根据键值进行解密
-     *
-     * @param data
-     * @param key  加密键byte数组
-     * @return
-     * @throws Exception
-     */
-    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
-        // 生成一个可信任的随机数源
-        SecureRandom sr = new SecureRandom();
-
-        // 从原始密钥数据创建DESKeySpec对象
-        DESKeySpec dks = new DESKeySpec(key);
-
-        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
-        SecretKey securekey = keyFactory.generateSecret(dks);
-
-        // Cipher对象实际完成解密操作
-        Cipher cipher = Cipher.getInstance(DES);
-
-        // 用密钥初始化Cipher对象
-        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
-
-        return cipher.doFinal(data);
-    }
-
 
 }
